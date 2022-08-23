e14b3a5dcf5e5464eefc0a099870afcec756aac9
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-08-22 23:05:45 -0500

    import logo and create header

---
+++ src/main/Header.tsx
@@ -0,0 +1,14 @@
+import logo from './GloboLogo.png';
+
+const Header = () => {
+    return (
+        <header className="row mb-4">
+            <div className="col-5">
+                <img src={logo} alt="logo" className='logo' />
+            </div>
+            <div className="col-7 mt-5 subtitle"></div>
+        </header>
+    )
+}
+
+export default Header;
