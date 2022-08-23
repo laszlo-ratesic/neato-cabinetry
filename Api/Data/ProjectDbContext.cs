1674532efb2a2d2cfe376d44d606042a22bd2beb
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-08-23 13:44:29 -0500

    configure the db using an override

--- Api/Data/ProjectDbContext.cs
+++ Api/Data/ProjectDbContext.cs
@@ -8,4 +8,19 @@ public class ProjectDbContext: DbContext
     public DbSet<ProjectEntity> Projects => Set<ProjectEntity>();
 
     // TODO: Configure which database to use
+    // Created an override in ProjectDbContext for OnConfiguring
+
+    // This function is called when the context is being created.
+    // It is used to configure the db (et al) to be used for this context
+    // DbContextOptionsBuilder will be used to configure the DbContext.
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Variables determine good place for db file
+        // These get the path to the local application data folder.
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+
+        // Configuring the DbContext to use a SQLite database.
+        optionsBuilder.UseSqlite($"Data Source={Path.Join(path, "projects.db")}");
+    }
 }
