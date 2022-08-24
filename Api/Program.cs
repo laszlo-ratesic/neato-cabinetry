316f2c7e4bdce8d1cfaa1d1fd49b95ec9f35cb3d
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-08-24 12:20:48 -0500

    gut starter data and reconfig endpoint to listen for /projects

--- Api/Program.cs
+++ Api/Program.cs
@@ -24,28 +24,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Access db by adding to lambda params bc it is registered
+// in the dep. injection container (which will provide us an instance)
+app.MapGet("/projects", (ProjectDbContext dbContext) =>
+    // all that's left is to return all project entities
+    // We can do this by simply accessing the Projects property,
+    // which is a collection of Project entities that will now be
+    // automatically serialized to JSON format
+    dbContext.Projects);
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
